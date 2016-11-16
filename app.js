4c0571140bf8e46cf415aa67587b92f745a6eb7f
Hudson Prestidge <charcon@ihug.co.nz>
2016-11-16 14:08:51 +1300

    updating works

--- app.js
+++ app.js
@@ -5,16 +5,9 @@ MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
   if(err) { return console.dir(err); }
 
   var collection = db.collection('test');
-  var doc1 = {'hello':'doc1'};
-  var doc2 = {'hello':'doc2'};
-  var lotsOfDocs = [{'hello':'doc3'}, {'hello':'doc4'}];
+  var doc = {mykey:1, fieldtoupdate:1};
 
-  collection.insert(doc1);
-
-  collection.insert(doc2, {w:1}, function(err, result) {
-    console.log(collection);
+  collection.insert(doc, {w:1}, function(err, result) {
+    collection.update({mykey:1}, {$set:{fieldtoupdate:2}}, {w:1}, function(err, result) {});
   });
-
-  collection.insert(lotsOfDocs, {w:1}, function(err, result) {});
-
-});
+})
