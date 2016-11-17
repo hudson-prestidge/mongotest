e98f07bd911a621b09b063936dbf7706955dca1b
Hudson Prestidge <charcon@ihug.co.nz>
2016-11-17 17:57:52 +1300

    tidied up

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 
-var MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect("mongodb://localhost:27017/exampleDb", function(err, db) {
+MongoClient.connect("mongodb://localhost:27017/exampleDb", (err, db) => {
   if(err) { return console.dir(err); }
 
-  var collection = db.collection('test');
-  var doc = {mykey:1, fieldtoupdate:1};
+  let collection = db.collection('test');
+  const doc = {mykey:1, fieldtoupdate:1};
 
   collection.insert(doc, {w:1},(err, result) => {
     collection.update({mykey:1}, {$set:{fieldtoupdate:2}}, {w:1},(err, result)=> {
-      var stream = collection.find({mykey:{$ne:2}}).stream();
+      let stream = collection.find({mykey:{$ne:2}}).stream();
       stream.on("data", function(item) {console.log(item);});
-      stream.on("end", function() {console.log("done streaming");});
-      collection.remove({mykey:1}, (err, result) => console.log("removed the thing!"))
-     })
-   })
+      stream.on("end", function() {
+        console.log("done streaming")
+        collection.remove({mykey:1}, (err, result) => console.log("removed the thing!"))
+      });
+    })
+  })
 })
