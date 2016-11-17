28330df977627034e228ce24aa09f0235f5e4468
Hudson Prestidge <charcon@ihug.co.nz>
2016-11-17 18:42:35 +1300

    added synchronous inserts

--- app.js
+++ app.js
@@ -8,7 +8,13 @@ MongoClient.connect("mongodb://localhost:27017/exampleDb", (err, db) => {
   const doc = {mykey:1, fieldtoupdate:1};
 
   collection.insert(doc, {w:1},(err, result) => {
+    console.log("added the thing!");
+
     collection.update({mykey:1}, {$set:{fieldtoupdate:2}}, {w:1},(err, result)=> {
+      console.log("updated the thing!");
+      collection.insert({mykey: 2, fieldtoupdate: 4})
+      collection.insert({mykey: 3, fieldtoupdate: 6})
+      collection.insert({mykey: 4, fieldtoupdate: 8})
       let stream = collection.find({mykey:{$ne:2}}).stream();
       stream.on("data", function(item) {console.log(item);});
       stream.on("end", function() {
